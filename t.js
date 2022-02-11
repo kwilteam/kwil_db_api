4b60fe486631545ea20572ba56d48517631e2b23
ttheolier <theolier@usc.edu>
2022-02-11 12:57:57 -0800

    test file for base 64

--- t.js
+++ t.js
@@ -1,14 +1,16 @@
 
 const KwilDB = require('./index.js')
 const { decryptKey } = require('./src/utils/decryptKey.js')
+const b64String = require('./testFiles/testFile')
 
 const kwilDB = KwilDB.createConnector({
     host: 'localhost',
     protocol: 'http',
     port: 1984,
     moat: 'test7',
-    apiKey: '][/riZP/ilT:qu&IA#Rhg[&e[[Ukp{]N',
-}, '($CezT*l2/~LA4/U2%@wANht55~{nir%')
+    apiKey: '9ydoed[GGu,KJ<m6Wm<FhrdHY;fl5bpX',
+}, 'o~>halS(K>UJ]ET1[Gh?Uo-#rr3Dp[=>')
+
 
 const testF = async () => {
     //console.log(await kwilDB.query(`SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE 'test';`))
@@ -16,7 +18,8 @@ const testF = async () => {
     //console.log(await KwilDB.createMoat('http://localhost:1984', 'test7', 'kwilSIG', '0xEF94BD30AA33de1677D7614D17aA39D493a485F1'))
     //console.log(await decryptKey('kwilSIG', '0xEF94BD30AA33de1677D7614D17aA39D493a485F1', 'U2FsdGVkX1+154CCJHU2FUrV7DOd1INDyidhHpf+ciebVG/s3R/uWVPh5PRy6A64L+o/n2Z19L/YUKixGRpl6A=='))
     //console.log(await kwilDB.createTable('table2' , {test: {}, username: 'varchar(100)'}))
-    kwilDB.query(`CREATE TABLE testtabl2(test_col text)`, true)
+    //kwilDB.query(`CREATE TABLE testtabl2(test_col text)`, true)
+    await kwilDB.storeJPEG('images/img.jpg',b64String,false);
 }
 
-//testF()
+testF()
