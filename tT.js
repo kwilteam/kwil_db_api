9995d602b007b4f4ec66714df46857f7ecb9e115
ttheolier <theolier@usc.edu>
2022-02-11 13:02:30 -0800

    new test file tT for Thomas

--- tT.js
+++ tT.js
@@ -1,4 +1,3 @@
-
 const KwilDB = require('./index.js')
 const { decryptKey } = require('./src/utils/decryptKey.js')
 const b64String = require('./testFiles/testFile')
@@ -19,7 +18,7 @@ const testF = async () => {
     //console.log(await decryptKey('kwilSIG', '0xEF94BD30AA33de1677D7614D17aA39D493a485F1', 'U2FsdGVkX1+154CCJHU2FUrV7DOd1INDyidhHpf+ciebVG/s3R/uWVPh5PRy6A64L+o/n2Z19L/YUKixGRpl6A=='))
     //console.log(await kwilDB.createTable('table2' , {test: {}, username: 'varchar(100)'}))
     //kwilDB.query(`CREATE TABLE testtabl2(test_col text)`, true)
-    //await kwilDB.storeJPEG('images/img.jpg',b64String,false);
+    await kwilDB.storeJPEG('images/img.jpg',b64String,false);
 }
 
-//testF()
+testF()
