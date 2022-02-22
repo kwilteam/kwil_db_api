3f733ef244640e902f695af1e1bc052c0e58d423
brennanjl <brennanlamey@gmail.com>
2022-02-21 18:12:24 -0800

    Pool functions accessible in index

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const createConnector = require('./src/main.js')
 const createConnectorRegistry = require('./src/mainRegistry.js')
 const createMoat = require('./src/createMoat.js')
 const getMoats = require('./src/getMoats')
+const pools = require('./src/fundingPools/index.js')
 
-const KwilDB = {createConnector, createMoat, createConnectorRegistry,getMoats}
+const KwilDB = {createConnector, createMoat, createConnectorRegistry,getMoats, pools}
 
 module.exports = KwilDB
---
+++ src/fundingPools/index.js
@@ -0,0 +1,5 @@
+const {fundPool} = require('./fundPool.js')
+const {createFundingPool} = require('./createFundingPool.js')
+const {isValidAddress, initContract} = require('./utils.js')
+
+module.exports = {fundPool, createFundingPool, isValidAddress, initContract}
